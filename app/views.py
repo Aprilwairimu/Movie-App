1b04972308e6294e0fb79fac2e0e79fed66b884d
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-04-29 12:16:56 +0300

    created macro for movie display

--- app/views.py
+++ app/views.py
@@ -40,7 +40,7 @@ def movie(id):
     movie = get_movie(id)
     title = f'{movie.title}'
     reviews = Review.get_reviews(movie.id)
-    return render_template('movie.html',title = title,movie = movie)
+    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
 
 @app.route('/search/<movie_name>')
 def search(movie_name):
@@ -67,6 +67,19 @@ def new_review(id):
 
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
+# def new_review(id):
+#     form = ReviewForm()
+#     movie = get_movie(id)
+
+#     if form.validate_on_submit():
+#         title = form.title.data
+#         review = form.review.data
+#         new_review = Review(movie.id,title,movie.poster,review)
+#         new_review.save_review()
+#         return redirect(url_for('movie',id = movie.id ))
+
+#     title = f'{movie.title} review'
+#     return render_template('new_review.html',title = title, review_form=form, movie=movie)
 
 
 
