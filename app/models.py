963fb3a378a81c579c1f848766dd348c5f09364c
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-04-29 14:50:01 +0300

    added a request function

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
+class Movie:
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    def __init__(self,id,title,overview,poster,vote_average,vote_count):
+        self.id =id
+        self.title = title
+        self.overview = overview
+        self.poster = "https://image.tmdb.org/t/p/w500/" + poster
+        self.vote_average = vote_average
+        self.vote_count = vote_count
+
 
 
 class Review:
@@ -29,4 +42,3 @@ class Review:
                 response.append(review)
 
         return response
-
