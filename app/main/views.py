8f7202c77b403c03785307b7d6752b97ca1af10c
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-04-29 14:28:52 +0300

    added a new file

--- app/main/views.py
+++ app/main/views.py
@@ -4,6 +4,7 @@ from app import app
 from .request import get_movies,get_movie,search_movie
 from .models import reviews
 from .forms import ReviewForm
+
 Review = reviews.Review
 
 
@@ -67,19 +68,7 @@ def new_review(id):
 
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
-# def new_review(id):
-#     form = ReviewForm()
-#     movie = get_movie(id)
 
-#     if form.validate_on_submit():
-#         title = form.title.data
-#         review = form.review.data
-#         new_review = Review(movie.id,title,movie.poster,review)
-#         new_review.save_review()
-#         return redirect(url_for('movie',id = movie.id ))
-
-#     title = f'{movie.title} review'
-#     return render_template('new_review.html',title = title, review_form=form, movie=movie)
 
 
 
