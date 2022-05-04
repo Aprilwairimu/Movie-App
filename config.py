4729e6ee4eadc2ab6cae820f68e9717570c4d8cf
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-04 13:22:20 +0300

    added an auth folder

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://april:2222@localhost/watchlist'
 
 class ProdConfig(Config):
     pass
@@ -18,33 +18,7 @@ config_options = {
 'production':ProdConfig
 }
 
-# class Config:
-#     '''
-#     General configuration parent class
-#     '''
-#     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
 
-
-
-# class ProdConfig(Config):
-#     '''
-#     Production  configuration child class
-
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-#     pass
-
-
-# class DevConfig(Config):
-#     '''
-#     Development  configuration child class
-
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-
-#     DEBUG = True
 
 
     
