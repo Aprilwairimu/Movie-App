e5ec42ecc209dbabe8b4da45a1481b7ecc5a1373
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-05 14:14:12 +0300

    added email configurations

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -12,13 +13,20 @@ login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
+mail = Mail()
 
+photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
 
     app = Flask(__name__)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+
+    mail.init_app(app)
+
+     # configure UploadSet
+    configure_uploads(app,photos)
 
     # Initializing flask extensions
     bootstrap.init_app(app)
--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 export MOVIE_API_KEY= 91ef6f114587961b584aa1424de4a67f
 export SECRET_KEY= kisha
+#.....
+export MAIL_USERNAME=wairimuapril@gmail.com
+export MAIL_PASSWORD=Aprilkisha21
 python3.9 manage.py server
