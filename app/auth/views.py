310418d172f0145f1b46d64e3e3ae975adc94011
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-05 13:01:01 +0300

    created a profile.html file

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,9 @@
 from flask import render_template,redirect,url_for,flash,request
-from . import auth
+from .. import auth
 from ..models import User
 from .forms import RegistrationForm,LoginForm
 from .. import db
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -20,7 +20,7 @@ def login():
     return render_template('auth/login.html',login_form = login_form,title=title)
   
 
-@auth.route('/register',methods = ["GET","POST"])
+@auth.route('/register','/logout',methods = ["GET","POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -30,3 +30,8 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
+
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
 
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from pip import main
 from . import main
 from ..requests import get_movies,get_movie,search_movie
-from ..models import Review
+from ..models import Review,User
 from .forms import ReviewForm
 from flask_login import login_required
 
@@ -71,7 +71,14 @@ def new_review(id):
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
 
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
 
 
 
